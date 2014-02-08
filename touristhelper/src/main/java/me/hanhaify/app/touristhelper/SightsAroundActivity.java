2e4c9619229cd62740fd39b2c48de77d3a809bb8
Han <hanhaify@gmail.com>
2014-02-08 21:36:58 +0800

    init project

---
+++ touristhelper/src/main/java/me/hanhaify/app/touristhelper/SightsAroundActivity.java
@@ -0,0 +1,12 @@
+package me.hanhaify.app.touristhelper;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SightsAroundActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setContentView(R.layout.activity_sights_around);
+    }
+}
